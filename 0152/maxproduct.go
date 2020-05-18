5f089d08e1b19e035669ac0522317bda8750a3bb
aschenmaker <aschen@cug.edu.cn>
2020-05-18 11:23:42 +0800

    20200518

---
+++ 0152/maxproduct.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nums = []int{1, -2, 3, -4, -1}
+	ret := maxProduct(nums)
+	fmt.Println(ret)
+}
+
+func maxProduct(nums []int) int {
+	maxF, minF, ans := nums[0], nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		mx, mn := maxF, minF
+		maxF = max(mx*nums[i], max(nums[i], mn*nums[i]))
+		minF = min(mn*nums[i], min(nums[i], mx*nums[i]))
+		ans = max(maxF, ans)
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
