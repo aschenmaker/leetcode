193c9d1fc4ab2823d5de9d1a0fb509b5c3f6b57c
aschenmaker <aschen@cug.edu.cn>
2020-05-31 20:56:42 +0800

    20200531

---
+++ 0101_symmetric-tree/symmetric-tree.go
@@ -0,0 +1,56 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func main() {
+	var root *TreeNode
+
+	isSymmetric(root)
+}
+
+// 递归方法
+
+func isSymmetric(root *TreeNode) bool {
+	return check(root, root)
+}
+
+func check(p, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	}
+	if p == nil || q == nil {
+		return false
+	}
+	return p.Val == q.Val && check(p.Left, q.Right) && check(p.Right, q.Left)
+}
+
+// 迭代方法
+func isSymmetricIteration(root *TreeNode) bool {
+	u, v := root, root
+	q := []*TreeNode{}
+	q = append(q, u)
+	q = append(q, v)
+	for len(q) > 0 {
+		u, v = q[0], q[1]
+		q = q[2:]
+		if u == nil && v == nil {
+			continue
+		}
+		if u == nil || v == nil {
+			return false
+		}
+		if u.Val != v.Val {
+			return false
+		}
+		q = append(q, u.Left)
+		q = append(q, v.Right)
+
+		q = append(q, u.Right)
+		q = append(q, v.Left)
+	}
+	return true
+}
