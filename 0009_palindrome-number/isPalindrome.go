8f583948228e672f59592a8309defc1553b2747c
aschenmaker <aschen@cug.edu.cn>
2020-06-10 19:59:26 +0800

    20200610

---
+++ 0009_palindrome-number/isPalindrome.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isPalindrome(121))
+}
+
+func isPalindrome(x int) bool {
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+	revertedNumber := 0
+	for x > revertedNumber {
+		revertedNumber = revertedNumber*10 + x%10
+		x /= 10
+
+	}
+	return x == revertedNumber || x == revertedNumber/10
+}
