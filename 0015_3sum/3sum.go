518eb165080dba9c3c8f5b4c55dff04f7cf23e6a
aschenmaker <aschen@cug.edu.cn>
2020-06-12 11:11:47 +0800

    20200612

---
+++ 0015_3sum/3sum.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	fmt.Println(threeSum(nums))
+}
+
+func threeSum(nums []int) [][]int {
+	n := len(nums)
+	sort.Ints(nums)
+	ans := make([][]int, 0)
+
+	// 枚举A
+	for first := 0; first < n; first++ {
+		if first > 0 && nums[first] == nums[first-1] {
+			continue
+		}
+
+		// 对应指针在数组最右端
+		third := n - 1
+		target := -1 * nums[first]
+		for second := first + 1; second < n; second++ {
+			if second > first+1 && nums[second] == nums[second-1] {
+				continue
+			}
+
+			for second < third && nums[second]+nums[third] > target {
+				third--
+			}
+			if second == third {
+				break
+			}
+
+			if nums[second]+nums[third] == target {
+				ans = append(ans, []int{nums[first], nums[second], nums[third]})
+			}
+		}
+	}
+	return ans
+}
