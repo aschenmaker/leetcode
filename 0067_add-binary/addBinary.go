9b38fed7d0a28b901742f8b6474b8522ac59e32f
aschenmaker <aschen@cug.edu.cn>
2020-06-23 11:23:47 +0800

    20200623

---
+++ 0067_add-binary/addBinary.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	a := "11"
+	b := "1"
+	fmt.Println(addBinary(a, b))
+}
+
+func addBinary(a, b string) string {
+	ans := ""
+	carry := 0
+	lenA, lenB := len(a), len(b)
+	n := max(lenA, lenB)
+
+	for i := 0; i < n; i++ {
+		if i < lenA {
+			carry += int(a[lenA-i-1] - '0')
+		}
+		if i < lenB {
+			carry += int(b[lenB-i-1] - '0')
+		}
+		ans = strconv.Itoa(carry%2) + ans
+		carry /= 2
+	}
+	if carry > 0 {
+		ans = "1" + ans
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
